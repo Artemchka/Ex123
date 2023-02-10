3bc57017d7beff6011747e59de463efa759c9ce6
Artem <shiryaev.artemi@gmail.com>
2023-02-10 23:20:44 +0700

    Добавление гирек

---
+++ ex6/Program.cs
@@ -0,0 +1,17 @@
+﻿int A = 1;
+int B = 3;
+int C = 6;
+int D = 8;
+int E = 4;
+
+int max = A;
+
+if (A > max) max =A;
+if (B > max) max =B;
+if (C > max) max =C;
+if (D > max) max =D;
+if (E > max) max =E;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
