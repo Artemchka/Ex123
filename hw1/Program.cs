9be970956ee0e1f6c5931d4350ff03f7db85400b
Artem <shiryaev.artemi@gmail.com>
2023-02-11 21:38:50 +0700

    Решил вторую задачу

---
+++ hw1/Program.cs
@@ -0,0 +1,17 @@
+﻿int A = 2;
+int B = 10;
+
+
+int max = A;
+int min = B;
+
+if (A > B) max =A;
+if (B > A) max =B;
+if (A < B) min =A;
+if (B < A) min =B;
+
+
+Console.Write("max= ");
+Console.WriteLine(max);
+Console.Write("min= ");
+Console.WriteLine(min);
