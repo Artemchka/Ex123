aa0f9aa55f2784e96ed109697a5bd1ec2eadc307
Artem <shiryaev.artemi@gmail.com>
2023-02-12 10:20:22 +0700

    Решил третью задачу

---
+++ hw3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число, не равное 0 ");
+int x = Convert.ToInt32(Console.ReadLine());
+if((x%2)==0)
+ {
+    Console.Write("Четное");
+ }
+else
+ {
+    Console.Write("Нечетное");
+ }
