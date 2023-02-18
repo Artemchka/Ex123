68fec080dd363f113be41cf3cea6540fe93e7cb9
Artem <shiryaev.artemi@gmail.com>
2023-02-18 21:09:39 +0700

    Решил 3 задачу с семинара 2

---
+++ sem2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+//Console.WriteLine("Введите число ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//if(num1 % 7 ==0 & num1 % 23 ==0)
+// Console.WriteLine("кратно");
+//else
+// Console.WriteLine("не кратно");
+
+// На входе принимает число
+
+int count = 0;
+int num = Convert.ToInt32(Console.ReadLine());
+while (num > 0){
+    num = num / 10;
+    count+=1;
+
+}
+Console.WriteLine(count);
