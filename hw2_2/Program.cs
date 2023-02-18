9e2a444d3aa83f8623d2c864ecd63a4c860be45f
Artem <shiryaev.artemi@gmail.com>
2023-02-18 21:39:47 +0700

    Решил 2 задачу ДЗ

---
+++ hw2_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+
+Console.WriteLine("Введите трехзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int num1 = num % 10; 
+while (num > 0){
+    num = num / 10;
+    count+=1;
+}
+if (count <3)
+{
+    Console.WriteLine(" Третьей цифры нет");
+}
+
+else
+{
+   Console.WriteLine(num1);
+}
