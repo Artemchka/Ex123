1f49a7a694459893589ac558005f903c7ce8e728
Artem <shiryaev.artemi@gmail.com>
2023-02-18 20:33:48 +0700

    Добавил решение двух задач со второго семинара

---
+++ sem2/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = new Random().Next(10, 100);
+Console.WriteLine($"Было сгенерировано число {num}");
+int x1 = num/10;
+int x2 = num % 10;
+if (x1>x2)
+  Console.WriteLine(x1);
+ else if(x1<x2)
+Console.WriteLine(x2);
+else
+Console.WriteLine("Обе цифры этого числа равны");
+
+for (int i=0;i<=10; i++)
+Console.Write($"{i}");
---
+++ sem2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу? которая будет принимать на вход 2 числа и выводить, является ли второе число кратным первому
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num2 % num1 ==0)
+ Console.WriteLine($"{num2} кратно {num1}");
+else
+{
+ int num3 = num2 % num1;
+ Console.WriteLine($"{num2} не кратно {num1} остатком является {num3}");
+}
