90571ce49ea2252d49865eeabb7506b7432e87f0
Artem <shiryaev.artemi@gmail.com>
2023-02-19 15:39:54 +0700

    решили задачу с квадратами

---
+++ sem3_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу, которая принимает на вход число и выдает таблицу квадратов чисел от 1 до числа (5=4 9 16 25)
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int x1 =x;
+string res ="";
+for (int i=0; i <= x; i++){
+ int tmp =i * i;
+ res += $"{tmp}, ";
+
+}
+
+Console.WriteLine(res);
