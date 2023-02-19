7fb35e6def452fbdb184afc9a933dc0e6327d34a
Artem <shiryaev.artemi@gmail.com>
2023-02-19 15:23:23 +0700

    Решил вторую задачу с семинара

---
+++ sem3_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая по заданному номеру четвеоти. показывает пиапазон возможных координат точек в этои четверти (Х и у).
+
+Console.WriteLine("Введите номер четверти ");
+int num = Convert.ToInt32(Console.ReadLine());
+CheckKoord(num);
+
+void CheckKoord(int num)
+{
+    if (num == 1)
+        Console.WriteLine("x > 0, y > 0");
+    else if (num == 4)
+        Console.WriteLine("x > 0, y < 0");
+    else if (num == 3)
+        Console.WriteLine("x < 0, y < 0");
+    else if (num == 2)
+        Console.WriteLine("x < 0, y > 0");
+    else
+        Console.WriteLine("Введены некорректные данные");
+}
