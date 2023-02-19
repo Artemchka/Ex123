e1ef34e148d0b17c79572e9e45fcd656f5d924db
Artem <shiryaev.artemi@gmail.com>
2023-02-19 15:14:23 +0700

    Добавил первую задачу с 3 семинара

---
+++ sem3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу которая принимает на вход координаты икс и игрик, причем  икс и игрик не равны нулю, и выдает номер четверти плоскости, в которой эта точка
+
+void CheckKoord(int x)
+{
+    if (x == 1)
+        Console.WriteLine("x > 0, y > 0");
+    else if (x == 4)
+        Console.WriteLine("x > 0, y < 0");
+    else if (x == 3)
+        Console.WriteLine("x < 0, y < 0");
+    else if (x == 2)
+        Console.WriteLine("x < 0, y > 0");
+    else
+        Console.WriteLine("Введены некорректные данные");
+}
+
+string CheckKoord2(int x)
+{
+    string res="Были введены некорректные данные";
+    if (x == 1)
+        res="x > 0, y > 0";
+    else if (x == 4)
+        res="x > 0, y < 0";
+    else if (x == 3)
+        res="x < 0, y < 0";
+    else if (x == 2)
+        res="x < 0, y > 0";
+    return res;
+}
+
+
+
+try
+{
+    Console.WriteLine("Введите координату Х");
+    int x = Convert.ToInt32(Console.ReadLine());
+    CheckKoord(x);
+    Console.WriteLine(CheckKoord2(x));
+}
+catch
+{
+    Console.WriteLine("Были введены некорректные данные");
+}
