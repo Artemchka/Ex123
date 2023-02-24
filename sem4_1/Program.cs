d4d55f3b6547a19714f56673a8972af270c830e6
Artem <shiryaev.artemi@gmail.com>
2023-02-24 22:38:14 +0700

    Списал решение 2 задач с семинара

---
+++ sem4_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите метод нахождения количества чисел в числе
+
+int Razr(int num)
+{
+    int count = 0;
+    while(num !=0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+try{
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Количество чисел в числе - {Razr(number)}");
+
+}
+catch
+{
+    Console.WriteLine(" Введены некорректные данные ");
+    
+}
