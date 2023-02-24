bd68a524edc38040b3355bcf5719b458ad5f8890
Artem <shiryaev.artemi@gmail.com>
2023-02-25 00:03:09 +0700

    Решил 3 задачи домашней работы

---
+++ hw4_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] array = new int[8];
+
+void PrintMass(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i=0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 9);
+        Console.Write($"{arr[i]} ");
+
+    }
+}
+PrintMass(array);
